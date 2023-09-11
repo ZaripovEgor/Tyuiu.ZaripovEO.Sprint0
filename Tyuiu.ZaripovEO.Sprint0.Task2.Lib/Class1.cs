2c5d21831ad14b5a21363b53b53c2eda6791bd96
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-09-11 18:45:58 +0500

    Task1

---
+++ Tyuiu.ZaripovEO.Sprint0.Task2.Lib/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.ZaripovEO.Sprint0.Task2.Lib
+{
+    public class Class1
+    {
+    }
+}
