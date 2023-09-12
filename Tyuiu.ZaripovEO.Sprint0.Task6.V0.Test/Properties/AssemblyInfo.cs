70beccf4c6cf2096ca26c4400d0aff7d440e73ca
Tyuiu.ZaripovEO@outlook.com <zaripov-egor@mail.ru>
2023-09-12 11:49:02 +0500

    Task6

---
+++ Tyuiu.ZaripovEO.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.ZaripovEO.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.ZaripovEO.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("5e37b0aa-e581-47ea-ac9a-ef77f4fb633d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.ZaripovEO.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ZaripovEO.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.ZaripovEO.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numArray = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Сумма элементов массива = " + DataService.AdditionArray(numArray));
+            Console.WriteLine("Разность элемента массива = " + DataService.SubtractionArray(numArray));
+            Console.WriteLine("Произведение элементов массива = " + DataService.MultiplicationArray(numArray));
+
+            Console.ReadKey();
+        }
+    }
+}
